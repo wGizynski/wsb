f7e3b0b38054966d6f29272e1d2f0fb5b8dc31eb
Wojciech Gizynski <w.gizynski@kl-pl-adm-04.kiel.com.pl.lokalne>
2020-03-07 13:12:06 +0100

    heredoc

--- Aplikacje internetowe/1_struktura.php
+++ Aplikacje internetowe/1_struktura.php
@@ -1,14 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
-        echo "text";
-        ?> 
+    <?php
+    $name = "test";
+    $surname = "test";
+    echo $name;
+    //heredoc
+    echo <<<SHOW
+         <hr>
+         Imię: $name <br>
+         Naziwsko: $surname
+         <hr>
+SHOW;
+
+    $text = <<<SHOW
+        <hr>
+        Imię: $name <br>
+        Naziwsko: $surname
+        <hr>s
+SHOW;
+    ?>
 </body>
+
 </html>

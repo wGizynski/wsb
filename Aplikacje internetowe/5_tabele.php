fc27502c5b6834d2a54e331d049eddacac2bb727
Wojciech Gizynski <w.gizynski@kiel.com.pl>
2020-03-23 08:57:44 +0100

    New

---
+++ Aplikacje internetowe/5_tabele.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+
+    <title></title>
+</head>
+
+<body>
+    <table id="table1">
+        <trx>
+            <td rowspan="3">1.1</td>
+            <td rowspan="2">1.2</td>
+            </tr>
+            <tr>
+            </tr>
+            <tr>
+                <td rowspan="3">2.2</td>
+            </tr>
+            <tr>
+                <td rowspan="2">3.1;</td>
+            </tr>
+            <tr>
+            </tr>
+            <tr>
+                <td rowspan="2" colspan="2">4.1;</td>
+            </tr>
+            <tr>
+            </tr>
+    </table>
+</body>
+
+</html>
